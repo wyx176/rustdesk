87306989d168289484c7b54ce1dce53eb4dd29cb
dev <32320103+wyx176@users.noreply.github.com>
2024-08-28 22:53:36 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.916b.cn"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.916b.cn"];
 pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
